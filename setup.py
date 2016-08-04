1828f233dca154ba710795a1ffab326f63412c59
Mikhail Yushkovskiy <mailto@zensecurity.su>
2016-08-04 16:22:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 
 
 def setup():
-    print(find_packages(exclude=['tests*', 'mod_utils*']))
+    print(find_packages(exclude=['*tests*', 'mod_utils*']))
     try:
         profiles_dir = 'w3af-repo/profiles'
 
